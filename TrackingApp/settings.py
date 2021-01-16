29aaa69c59c9320232b8f4f31feb5421f754c952
Mohammed Shoaib <shoaibbilal101@gmail.com>
2021-01-16 13:49:57 +0500

    123?

--- TrackingApp/settings.py
+++ TrackingApp/settings.py
@@ -131,4 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
+
+
+# Upload Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
+MEDIA_URL = '/upload/'
